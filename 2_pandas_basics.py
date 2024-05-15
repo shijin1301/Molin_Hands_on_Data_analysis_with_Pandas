6177c8995405a08b16e64a78f7bcd8d530a38364
shijin1301 <shijinars@gmail.com>
2024-05-15 16:39:29 +0530

    head function

--- 2_pandas_basics.py
+++ 2_pandas_basics.py
@@ -2,7 +2,8 @@
 # import pandas as pd
 import numpy as np
 
-data = np.genfromtxt('E:\Practice\Python\Hands-On-Data-Analysis-with-Pandas-2nd-edition\ch_02\data\example_data.csv',
-                    delimiter=';', names=True, dtype=None, encoding = 'UTF-8')
+data = np.genfromtxt(
+    'E:\Practice\Python\Hands-On-Data-Analysis-with-Pandas-2nd-edition\ch_02\data\example_data.csv',
+    delimiter=';', names=True, dtype=None, encoding='UTF-8')
 
-print(data)
+print(data[:5])
